580d380751f1845073fc8aecc277e331536f8de7
Ranjan <ranjankumar@techis.io>
2022-07-30 16:14:31 +0530

    done

---
+++ README.md
@@ -0,0 +1,2 @@
+# Django-forum
+A forum
---
+++ static/js/posts.js
@@ -0,0 +1,9 @@
+//JavaScript file////
+//////////////////
+
+$(function () {
+  $(".js-menu-icon").click(function () {
+    $(this).next().toggle();
+  })
+})
+
